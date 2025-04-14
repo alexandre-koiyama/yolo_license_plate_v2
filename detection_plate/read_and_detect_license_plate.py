6d4d41f4717d1b27d56dc0d97bf06071009dc6ea
Alexandre <alexandre.koiyama@outlook.com>
2025-04-14 12:07:50 -0500

    Update read_and_detect_license_plate.py

--- detection_plate/read_and_detect_license_plate.py
+++ detection_plate/read_and_detect_license_plate.py
@@ -1,10 +1,21 @@
-
 import cv2
 import numpy as np
 from ultralytics import YOLO
-import easyocr
+from paddleocr import PaddleOCR
 
-cap = cv2.VideoCapture('VIDEO_PLATE.mp4')
+def ocr_with_paddle(img):
+    finaltext = ''
+    ocr = PaddleOCR(lang='ch', use_angle_cls=True)
+    img_path = 'exp.jpeg'
+    result = ocr.ocr(img)
+
+    for i in range(len(result[0])):
+        text = result[0][i][1][0]
+        finaltext += ' '+ text
+    return finaltext
+
+
+cap = cv2.VideoCapture('video_original.mp4')
 
 model = YOLO('best.pt')
 
@@ -13,8 +24,9 @@ width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 fps = int(cap.get(cv2.CAP_PROP_FPS))
 
-out_put = cv2.VideoWriter('output_dd.mp4', fourcc, fps, (width, height))
-reader = easyocr.Reader(["en"])
+out_put = cv2.VideoWriter('output.mp4', fourcc, fps, (width, height))
+
+
 
 while cap.isOpened():
     ret, frame = cap.read()
@@ -33,24 +45,17 @@ while cap.isOpened():
             class_name = model.model.names[class_id]
 
             cv2.rectangle(frame, (x1, y1), (x2, y2), (60, 92, 255), 3)
-            
-            #MASK
+
             plate_crop = frame[y1:y2, x1:x2]
-            plate_crop = cv2.resize(plate_crop, (0, 0), fx=10, fy=10)
-            gray = cv2.cvtColor(plate_crop, cv2.COLOR_BGR2GRAY)
-            clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(32, 32))
-            gray = clahe.apply(gray)
-            filtered = cv2.bilateralFilter(gray, d=12, sigmaColor=75, sigmaSpace=75)
-            _, thresh = cv2.threshold(filtered, 100, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-            kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
-            morph = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+            plate_crop = cv2.resize(plate_crop, (0, 0), fx=6, fy=6)
 
-            output = reader.readtext(morph)
-            for out in output:
-                text_bbox, text, text_score = out
-                if text_score > 0.3:
-                    cv2.putText(frame, text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
-
+            try:
+              text_out = ocr_with_paddle(plate_crop)
+              cv2.putText(frame, text_out, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+              #print(text_out)
+            except:
+              pass
+            
     out_put.write(frame)
 
 cap.release()
